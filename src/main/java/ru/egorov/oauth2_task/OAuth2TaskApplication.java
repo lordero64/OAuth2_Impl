16d5e1028b7dcdeb9cde9c706656558d109641ae
Egor Egorov <lordero@list.ru>
2026-03-24 16:51:24 +0500

    first commit

---
+++ src/main/java/ru/egorov/oauth2_task/OAuth2TaskApplication.java
@@ -0,0 +1,13 @@
+package ru.egorov.oauth2_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2TaskApplication.class, args);
+	}
+
+}
